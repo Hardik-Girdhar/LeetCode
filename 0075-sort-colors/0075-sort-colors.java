2bb04e360f1a639728abe046ef0d78281c176822
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-03-06 16:53:09 +0530

    Time: 0 ms (100.00%), Space: 42 MB (14.85%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,31 +1,32 @@
 class Solution {
     public void sortColors(int[] nums) {
         // DUTCH ALGORITHM
-        int m = 0;
-        int s = 0;
-        int e = nums.length-1;
-        while(m<=e)
-        {
-            if(nums[m] == 0)
-            {
-                int temp = nums[s];
-                nums[s] = nums[m];
-                nums[m] = temp;
-                s++;
-                m++;
-            }
-            else if(nums[m] == 1)
-            {
-                m++;
-            }
-            else if(nums[m] == 2)
-            {
-                int temp = nums[e];
-                nums[e] = nums[m];
-                nums[m] = temp;
-                e--;
-                // m++;
-            }
-        }
+        // int m = 0;
+        // int s = 0;
+        // int e = nums.length-1;
+        // while(m<=e)
+        // {
+        //     if(nums[m] == 0)
+        //     {
+        //         int temp = nums[s];
+        //         nums[s] = nums[m];
+        //         nums[m] = temp;
+        //         s++;
+        //         m++;
+        //     }
+        //     else if(nums[m] == 1)
+        //     {
+        //         m++;
+        //     }
+        //     else if(nums[m] == 2)
+        //     {
+        //         int temp = nums[e];
+        //         nums[e] = nums[m];
+        //         nums[m] = temp;
+        //         e--;
+        //         // m++;
+        //     }
+        // }
+        Arrays.sort(nums);
     }
 }
