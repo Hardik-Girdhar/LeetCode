15bd0572a90b30c05d406cce107b30ae2869b361
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-03-06 15:38:48 +0530

    Time: 2 ms (84.35%), Space: 46.3 MB (27.13%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,75 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // int j=-1;
+        // for(int i = 0 ; i <nums.length ; i++)
+        // {
+        //     if(nums[i] == 0)
+        //     {
+        //         j=i;
+        //         break;
+        //     }
+        // }
+        // if(j==-1)
+        // {
+        //     return;
+        // }
+        // for(int i=j+1 ; i<nums.length ; i++)
+        // {
+        //     if(nums[i] != 0)
+        //     {
+        //         int temp = nums[i];
+        //         nums[i] = nums[j];
+        //         nums[j] = temp;
+        //         j++;
+        //     }
+        // }
+        int j = -1;
+        for(int i = 0 ; i < nums.length; i++)
+        {
+            if(nums[i] == 0 )
+            {
+                j = i;
+                break;
+            }
+        }
+        if(j == -1)
+        {
+            return;
+        }
+        for(int i = j+1 ; i < nums.length ; i++){
+            if(nums[i] != 0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         for(int i = 0 ; i<nums.length ; i++)
+//         {
+//             if(nums[i] != 0)
+//             {
+//                 list.add(nums[i]); // list without zeros
+//             }
+//         }
+//         for(int i=0; i<list.size() ; i++)
+//         {
+//             nums[i] = list.get(i);
+//         }
+//         for(int i = list.size() ; i<nums.length ; i++)
+//         {
+//             nums[i] = 0;
+//         }
+
+//         for(int i=0 ; i<nums.length ; i++)
+//         {
+//             System.out.print(nums[i]);
+//         }
+//     }
+// }
