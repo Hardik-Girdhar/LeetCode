db810236b1d8399f01632417810c0fa4fe9b6b98
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-03-15 14:23:15 +0530

    Time: 23 ms (92.31%), Space: 46.6 MB (32.09%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,20 +1,43 @@
+// OPTIMAL - HASH - n
 class Solution {
     public int subarraySum(int[] nums, int key) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum =0;
         int count=0;
-        for(int i=0 ; i<nums.length; i++){
-                int sum =0 ;
-            for(int j =i;j<nums.length;j++){
-                    sum = sum + nums[j];
-                if(sum == key)
-                {
-                    count++;
-                }
+        for(int i =0;i<nums.length; i++)
+        {
+            sum = sum+nums[i];
+            int remove = sum - key;
+            if(map.containsKey(remove))
+            {
+                count = count+map.get(remove);
             }
+            map.put(sum , map.getOrDefault(sum , 0)+1);
         }
         return count;
     }
 }
 
+// BRUTE FORCE - n^2
+// class Solution {
+//     public int subarraySum(int[] nums, int key) {
+//         int count=0;
+//         for(int i=0 ; i<nums.length; i++){
+//                 int sum =0 ;
+//             for(int j =i;j<nums.length;j++){
+//                     sum = sum + nums[j];
+//                 if(sum == key)
+//                 {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+// BRUTE FORCE  - n^3
 // class Solution {
 //     public int subarraySum(int[] nums, int key) {
 //         int count=0;
