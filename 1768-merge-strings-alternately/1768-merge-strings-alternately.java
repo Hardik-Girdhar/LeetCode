4862e25889cbe6cac571d9df0001e52214dc2870
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-03-27 09:59:47 +0530

    Time: 1 ms (84.66%), Space: 41 MB (98.72%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,28 +1,67 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int a=word1.length();
-        int b=word2.length();
-        String s="";
-        int n = Math.min(a,b);
-        for(int i =0;i<n;i++)
+        StringBuilder sb = new StringBuilder();
+        
+        int a = word1.length();
+        int b = word2.length();
+        int i=0;
+        int j=0;
+        boolean flag = true;
+        while(i<a && j<b)
         {
-            s = s+word1.charAt(i);
-            s = s+word2.charAt(i);
+            if(flag)
+            {
+                sb.append(word1.charAt(i));
+                i++;
+            }
+            else
+            {
+                sb.append(word2.charAt(j));
+                j++;
+            }
+            flag = !flag;
         }
-        if(a>b)
+        while(i<a)
         {
-            for(int i=n;i<a;i++)
-                {
-                    s = s+word1.charAt(i);
-                }
+            sb.append(word1.charAt(i));
+            i++;
         }
-        else
+        while(j<b)
         {
-            for(int i=n;i<b;i++)
-                {
-                    s = s+word2.charAt(i);
-                }
+            sb.append(word2.charAt(j));
+            j++;
         }
-        return s;
+        
+        return sb.toString();
     }
 }
+
+
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+//         int a=word1.length();
+//         int b=word2.length();
+//         String s="";
+//         int n = Math.min(a,b);
+//         for(int i =0;i<n;i++)
+//         {
+//             s = s+word1.charAt(i);
+//             s = s+word2.charAt(i);
+//         }
+//         if(a>b)
+//         {
+//             for(int i=n;i<a;i++)
+//                 {
+//                     s = s+word1.charAt(i);
+//                 }
+//         }
+//         else
+//         {
+//             for(int i=n;i<b;i++)
+//                 {
+//                     s = s+word2.charAt(i);
+//                 }
+//         }
+//         return s;
+//     }
+// }
