8a29be15f7fc974e6b8d4326d62220c72c4ba6f8
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-03-31 20:33:51 +0530

    Time: 1 ms (98.62%), Space: 41.3 MB (62.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,50 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> st = new Stack<>();
+//         for(int i=0;i<s.length();i++)
+//         {
+//             char c = s.charAt(i);
+//             if(c=='(' || c=='{' || c=='[')
+//             {
+//                 st.push(c);
+//             }
+//             else
+//             {
+//                 if(st.isEmpty())
+//                 {
+//                     return false;
+//                 }
+//                 else if(c==')' && st.peek() !='(') return false;
+//                 else if(c==']' && st.peek() !='[') return false;
+//                 else if(c=='}' && st.peek() !='{') return false;
+//                 st.pop();
+//             }
+//         }
+//         return st.isEmpty();
+//     }
+// }
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        for(int i=0;i<s.length();i++)
+        for(char c:s.toCharArray())
         {
-            char c = s.charAt(i);
-            if(c=='(' || c=='{' || c=='[')
+            if(c=='(')
             {
-                st.push(c);
+                st.push(')');
             }
-            else
+            else if(c=='{')
             {
-                if(st.isEmpty())
-                {
-                    return false;
-                }
-                else if(c==')' && st.peek() !='(') return false;
-                else if(c==']' && st.peek() !='[') return false;
-                else if(c=='}' && st.peek() !='{') return false;
-                st.pop();
+                st.push('}');
+            }
+            else if(c=='[')
+                
+            {
+                st.push(']');
+            }
+            else if(st.isEmpty() || st.pop() != c)
+            {
+                return false;
             }
         }
         return st.isEmpty();
