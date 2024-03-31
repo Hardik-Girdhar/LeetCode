8e48c4eb7484ce8f238941f9308dfec04bd91444
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-03-31 19:19:44 +0530

    Time: 2 ms (84.76%), Space: 46.5 MB (6.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,53 +1,53 @@
 
-// class Solution {
-//     public void moveZeroes(int[] nums) {
-//         int j=-1;
-//         for(int i = 0 ; i <nums.length ; i++)
-//         {
-//             if(nums[i] == 0)
-//             {
-//                 j=i;
-//                 break;
-//             }
-//         }
-//         if(j==-1)
-//         {
-//             return;
-//         }
-//         for(int i=j+1 ; i<nums.length ; i++)
-//         {
-//             if(nums[i] != 0)
-//             {
-//                 int temp = nums[i];
-//                 nums[i] = nums[j];
-//                 nums[j] = temp;
-//                 j++;
-//             }
-//         }
-//     }
-// }
-
 class Solution {
     public void moveZeroes(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<nums.length;i++)
+        int j=-1;
+        for(int i = 0 ; i <nums.length ; i++)
         {
-            if(nums[i] !=0)
+            if(nums[i] == 0)
             {
-                list.add(nums[i]);
+                j=i;
+                break;
             }
         }
-        for(int i=0;i<list.size();i++)
+        if(j==-1)
         {
-            nums[i] = list.get(i);
+            return;
         }
-        for(int i=list.size();i<nums.length ;i++)
+        for(int i=j+1 ; i<nums.length ; i++)
         {
-            nums[i]=0;
-        }
-        for(int i=0;i<nums.length;i++)
-        {
-            System.out.print(nums[i]);
+            if(nums[i] != 0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
         }
     }
 }
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         List<Integer> list = new ArrayList<>();
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i] !=0)
+//             {
+//                 list.add(nums[i]);
+//             }
+//         }
+//         for(int i=0;i<list.size();i++)
+//         {
+//             nums[i] = list.get(i);
+//         }
+//         for(int i=list.size();i<nums.length ;i++)
+//         {
+//             nums[i]=0;
+//         }
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             System.out.print(nums[i]);
+//         }
+//     }
+// }
