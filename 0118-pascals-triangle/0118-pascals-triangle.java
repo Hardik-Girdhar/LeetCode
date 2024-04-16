a8390d2a9289c51823619f666619116f7812f9c1
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-04-16 12:01:30 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (69.41%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,71 +1,45 @@
 // // OPTIMAL SOLUTION
+class Solution {
+    public List<Integer> rowelement(int row){
+        List<Integer> list = new ArrayList<>();
+        int res=1;
+        list.add(1);
+        for(int i=1;i<row; i++)
+        {
+            res = res * (row-i);
+            res = res / i;
+            list.add(res);
+        }
+        return list;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> main = new ArrayList<>();
+        for(int i=1 ; i<=numRows; i++){
+            main.add(rowelement(i));
+        }
+        return main;
+    }
+}
+
+//  BRUTE FORCE
 // class Solution {
-//     public List<Integer> rowelement(int row){
-//         List<Integer> list = new ArrayList<>();
-//         int res=1;
-//         list.add(1);
-        // for(int i=1;i<row; i++)
-//         {
-//             res = res * (row-i);
+//     public static int ncr(int n,int r){
+//         long res=1;
+//         for(int i=1;i<=r;i++){
+//             res = res * (n-i+1);
 //             res = res / i;
-//             list.add(res);
 //         }
-//         return list;
+//         return (int)res;
 //     }
 //     public List<List<Integer>> generate(int numRows) {
 //         List<List<Integer>> main = new ArrayList<>();
-//         for(int i=1 ; i<=numRows; i++){
-//             main.add(rowelement(i));
+//         for(int i=1; i<=numRows; i++){
+//             List<Integer> list = new ArrayList<>();
+//             for(int j=1; j<=i; j++){
+//                 list.add(ncr(i-1,j-1));
+//             }
+//             main.add(list);
 //         }
 //         return main;
 //     }
 // }
-
-// //  BRUTE FORCE
-// // class Solution {
-// //     public static int ncr(int n,int r){
-// //         long res=1;
-// //         for(int i=1;i<=r;i++){
-// //             res = res * (n-i+1);
-// //             res = res / i;
-// //         }
-// //         return (int)res;
-// //     }
-// //     public List<List<Integer>> generate(int numRows) {
-// //         List<List<Integer>> main = new ArrayList<>();
-// //         for(int i=1; i<=numRows; i++){
-// //             List<Integer> list = new ArrayList<>();
-// //             for(int j=1; j<=i; j++){
-// //                 list.add(ncr(i-1,j-1));
-// //             }
-// //             main.add(list);
-// //         }
-// //         return main;
-// //     }
-// // }
-
-class Solution {
-    public List<List<Integer>> generate(int row){
-        List<List<Integer>> main = new ArrayList<>();
-        for(int i=1;i<=row;i++)
-        {
-            ArrayList<Integer> temp = new ArrayList<>();
-            for(int j=1;j<=i;j++)
-            {
-                temp.add(ncr(i-1,j-1));
-            }
-            main.add(temp);
-        }
-        return main;
-    }
-    public int ncr(int n,int r)
-    {
-        long ans = 1;
-        for(int i=0;i<r;i++)
-        {
-            ans = ans * (n-i);
-            ans = ans / (i+1);
-        }
-        return (int)ans;
-    }
-}
