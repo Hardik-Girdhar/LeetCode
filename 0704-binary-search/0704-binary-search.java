eade7c8d6b11d62347de02ecb4750555ba1f234a
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-04-16 12:40:21 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (21.16%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,12 +1,36 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++)
+        int s=0;
+        int e=nums.length-1;
+        while(s<=e)
         {
-            if(nums[i] == target)
+            int mid = s+(e-s)/2;
+            if(nums[mid] == target)
             {
-                return i;
+                return mid;
+            }
+            else if(nums[mid] > target)
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
             }
         }
         return -1;
     }
 }
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i] == target)
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
