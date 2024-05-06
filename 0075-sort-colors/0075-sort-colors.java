5f2b00d8b3b3d40207cc996dc3035cbb989d3fbb
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-05-06 12:02:42 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (47.47%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,38 +1,38 @@
-class Solution {
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);
-    }
-}
-
 // class Solution {
 //     public void sortColors(int[] nums) {
-//         int s=0;
-//         int m=0;
-//         int e=nums.length-1;
-        
-//         while(m<=e)
-//         {
-//             if(nums[m]==0)
-//             {
-//                 swap(nums,m,s);
-//                 m++;
-//                 s++;
-//             }
-//             else if(nums[m]==1)
-//             {
-//                 m++;
-//             }
-//             else
-//             {
-//                 swap(nums,m,e);
-//                 e--;
-//             }
-//         }
-//     }
-//     public void swap(int[] num,int a,int b)
-//     {
-//         int temp=num[a];
-//         num[a]=num[b];
-//         num[b]=temp;
+//         Arrays.sort(nums);
 //     }
 // }
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int s=0;
+        int m=0;
+        int e=nums.length-1;
+        
+        while(m<=e)
+        {
+            if(nums[m]==0)
+            {
+                swap(nums,m,s);
+                m++;
+                s++;
+            }
+            else if(nums[m]==1)
+            {
+                m++;
+            }
+            else
+            {
+                swap(nums,m,e);
+                e--;
+            }
+        }
+    }
+    public void swap(int[] num,int a,int b)
+    {
+        int temp=num[a];
+        num[a]=num[b];
+        num[b]=temp;
+    }
+}
