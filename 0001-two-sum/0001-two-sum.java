e8d636be0903ce1c4a02af4bf02a2e80b5a18c5a
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-05-06 11:59:45 +0530

    Time: 2 ms (97.64%), Space: 45.3 MB (8.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,29 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        // Arrays.sort(nums);
-        // int s=0;
-        // int e = nums.length-1;
-        // int ans[] = {-1,-1};
-        // while(s<=e)
-        // {
-        //     int sum = nums[s] + nums[e];
-        //     if(sum == target)
-        //     {
-        //         return new int[]{s,e};
-        //     }
-        //     else if(sum<target)
-        //     {
-        //         s++;
-        //     }
-        //     else
-        //     {
-        //         e--;
-        //     }
-        // }
-        // return ans;
-
-
         int ans[] = new int[2];   // hashing n   // better approach
         ans[0]=ans[1]=-1;
         HashMap<Integer,Integer> map = new HashMap<>();
@@ -40,18 +16,5 @@ class Solution {
             map.put(nums[i],i);
         }
         return ans;
-        
-
-        // for(int i=0 ; i<nums.length ; i++)     // BRUTE FORCE n^2  
-        // {
-        //     for(int j=i+1 ; j<nums.length ; j++)
-        //     {
-        //         if(target == nums[i] + nums[j])
-        //         {
-        //             return new int[]{i,j};
-        //         }
-        //     }
-        // }
-        // return null;
     }
 }
