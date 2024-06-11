a1567326340eee71332ce24749238f377cb68ef2
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-11 15:04:31 +0530

    Time: 2 ms (65.06%), Space: 41.5 MB (97.97%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,18 +1,36 @@
+// class Solution {
+//     public int[] relativeSortArray(int[] arr1, int[] arr2) {
+//         int count=0;
+//         for(int i=0;i<arr2.length;i++){
+//             for(int j=0;j<arr1.length;j++){
+//                 if(arr1[j] == arr2[i]){
+//                     int temp = arr1[count];
+//                     arr1[count] = arr1[j];
+//                     arr1[j] = temp;
+//                     count++;
+//                 }
+//             }
+//         }
+        
+//         Arrays.sort(arr1,count,arr1.length);
+//         return arr1;
+//     }
+// }
+
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int count=0;
+        int a=0;
         for(int i=0;i<arr2.length;i++){
             for(int j=0;j<arr1.length;j++){
-                if(arr1[j] == arr2[i]){
-                    int temp = arr1[count];
-                    arr1[count] = arr1[j];
+                if(arr2[i] == arr1[j]){
+                    int temp = arr1[a];
+                    arr1[a] = arr1[j];
                     arr1[j] = temp;
-                    count++;
+                    a++;
                 }
             }
         }
-        
-        Arrays.sort(arr1,count,arr1.length);
+        Arrays.sort(arr1,a,arr1.length);
         return arr1;
     }
 }
