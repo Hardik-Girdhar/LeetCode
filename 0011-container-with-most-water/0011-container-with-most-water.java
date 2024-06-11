7cfd43e9c2d6fbd925e9e54a8f21552fe311dd44
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-11 15:22:09 +0530

    Time: 5 ms (67.63%), Space: 58.1 MB (15.22%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,20 +1,40 @@
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int max = 0;
+//         int l=0;
+//         int r=height.length-1;
+//         while(l<r){
+//             int length = Math.min(height[l],height[r]);
+//             int breadth = r-l;
+//             int curr = length * breadth;
+//             max = Math.max(max,curr);
+//             if(height[l]<=height[r])
+//             {
+//                 l++;
+//             }
+//             else{
+//                 r--;
+//             }
+//         }
+//         return max;
+//     }
+// }
+
 class Solution {
     public int maxArea(int[] height) {
-        int i=0;
-        int j=height.length-1;
-        int curr=0;
         int max=0;
-        while(i<=j)
-        {
-            curr=(j-i)*(Math.min(height[i],height[j]));
-            max=Math.max(curr,max);
-            if(height[i]>height[j])
-            {
-                j--;
+        int l=0;
+        int r=height.length-1;
+        while(l<r){
+            int length = Math.min(height[l],height[r]);
+            int breadth = r-l;
+            int curr = length*breadth;
+            max = Math.max(curr,max);
+            if(height[l]<height[r]){
+                l++;
             }
-            else
-            {
-                i++;
+            else{
+                r--;
             }
         }
         return max;
