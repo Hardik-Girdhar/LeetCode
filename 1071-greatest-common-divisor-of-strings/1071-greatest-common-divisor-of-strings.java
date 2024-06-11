30415e9c42a139e2b5b2b5583e12b778f624eaf9
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-11 22:49:03 +0530

    Time: 1 ms (72.60%), Space: 41.7 MB (94.83%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,25 +1,37 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        if((str1 + str2).equals(str2+str1))
-        {
-            int a=str1.length();
-            int b=str2.length();
-            int gcd = gcd(a,b);
-            return str1.substring(0,gcd);
-        }
-        else
-        {
+        if(!(str1+str2).equals(str2+str1)){
             return "";
         }
+        int a=str1.length();
+        int b=str2.length();
+        int gcd=gcd(a,b);
+        return str1.substring(0,gcd);
     }
-    public int gcd(int a, int b)
-    {
-        while(b % a != 0)
-        {
-            int r = b%a;
-            b=a;
-            a=r;
+    static int gcd(int a,int b){
+        while(a%b != 0){
+            int rem = a%b;
+            a=b;
+            b=rem;
         }
-        return a;
+        return b;
     }
 }
+
+
+// class Solution {
+//     public String gcdOfStrings(String str1, String str2) {
+//         if(!(str1+str2).equals(str2+str1)){
+//             return "";
+//         }
+//         HashSet<Character> set = new HashSet<>();
+//         for(int i=0;i<str2.length();i++){
+//             set.add(str2.charAt(i));
+//         }
+//         StringBuilder str = new StringBuilder();
+//         for(char ele:set){
+//             str.append(ele);
+//         }
+//         return str.toString();
+//     }
+// }
