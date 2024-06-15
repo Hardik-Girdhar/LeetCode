2151ab266b24ffda88e7996b1d67d6394b6e704f
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-15 23:38:03 +0530

    Time: 5 ms (67.17%), Space: 57.1 MB (82.42%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,94 +1,19 @@
-// class Solution {
-//     public int maxArea(int[] height) {
-//         int max = 0;
-//         int l=0;
-//         int r=height.length-1;
-//         while(l<r){
-//             int length = Math.min(height[l],height[r]);
-//             int breadth = r-l;
-//             int curr = length * breadth;
-//             max = Math.max(max,curr);
-//             if(height[l]<=height[r])
-//             {
-//                 l++;
-//             }
-//             else{
-//                 r--;
-//             }
-//         }
-//         return max;
-//     }
-// }
-
-// class Solution {
-//     public int maxArea(int[] height) {
-//         int max=0;
-//         int l=0;
-//         int r=height.length-1;
-//         while(l<r){
-//             int length = Math.min(height[l],height[r]);
-//             int breadth = r-l;
-//             int curr = length*breadth;
-//             max = Math.max(curr,max);
-//             if(height[l]<height[r]){
-//                 l++;
-//             }
-//             else{
-//                 r--;
-//             }
-//         }
-//         return max;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Solution {
     public int maxArea(int[] height) {
         int max = 0;
         int l=0;
         int r=height.length-1;
         while(l<r){
-            int length = r-l;
-            int breadth = Math.min(height[l],height[r]);
-            int curr = length*breadth;
-            max = Math.max(curr,max);
-            
-            if(height[l]>=height[r]){
-                r--;
+            int length = Math.min(height[l],height[r]);
+            int breadth = r-l;
+            int curr = length * breadth;
+            max = Math.max(max,curr);
+            if(height[l]<=height[r])
+            {
+                l++;
             }
             else{
-                l++;
+                r--;
             }
         }
         return max;
