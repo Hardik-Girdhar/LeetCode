12802635a2311036274a225ef5d1db8694736cc9
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-15 23:21:37 +0530

    Time: 2 ms (83.52%), Space: 46 MB (54.05%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,53 +1,22 @@
-
 class Solution {
     public void moveZeroes(int[] nums) {
         int j=-1;
-        for(int i = 0 ; i <nums.length ; i++)
-        {
-            if(nums[i] == 0)
-            {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == 0){
                 j=i;
                 break;
             }
         }
-        if(j==-1)
-        {
-            return;
+        if(j==-1){
+            return ;
         }
-        for(int i=j+1 ; i<nums.length ; i++)
-        {
-            if(nums[i] != 0)
-            {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        for(int i=j+1;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
                 j++;
             }
         }
     }
 }
-
-// class Solution {
-//     public void moveZeroes(int[] nums) {
-//         List<Integer> list = new ArrayList<>();
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             if(nums[i] !=0)
-//             {
-//                 list.add(nums[i]);
-//             }
-//         }
-//         for(int i=0;i<list.size();i++)
-//         {
-//             nums[i] = list.get(i);
-//         }
-//         for(int i=list.size();i<nums.length ;i++)
-//         {
-//             nums[i]=0;
-//         }
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             System.out.print(nums[i]);
-//         }
-//     }
-// }
