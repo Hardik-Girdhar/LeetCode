c1ae55a73df099c2c8edd9d08a0043407628b2d2
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-18 11:26:48 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (20.31%) - LeetHub

--- 0330-patching-array/0330-patching-array.java
+++ 0330-patching-array/0330-patching-array.java
@@ -1,55 +1,15 @@
-// class Solution {
-//     public int minPatches(int[] nums, int n) {
-//         long miss = 1;
-//         int result = 0;
-//         int i = 0;
-
-//         while (miss <= n) {
-//             if (i < nums.length && nums[i] <= miss) {
-//                 miss += nums[i];
-//                 i++;
-//             } else {
-//                 miss += miss;
-//                 result++;
-//             }
-//         }
-
-//         return result;
-//     }
-// }
-
-// class Solution {
-//     public int minPatches(int[] nums, int n) {
-//         int patch=0;
-//         long max_reach=0;
-//         int i=0;
-//         while(max_reach < n){
-//             if(i<nums.length && nums[i] <= max_reach+1){
-//                 max_reach += nums[i];
-//                 i++;
-//             }
-//             else{
-//                 max_reach += (max_reach+1);
-//                 patch++;
-//             }
-//         }
-//         return patch;
-//     }
-// }
-
-
 class Solution {
     public int minPatches(int[] nums, int n) {
-        int patch = 0;
-        long max_reach = 0;
+        int patch=0;
+        long max_reach=0;
         int i=0;
-        while(max_reach<n){
-            if(i<nums.length && nums[i]<=max_reach+1){
+        while(max_reach < n){
+            if(i<nums.length && nums[i] <= max_reach+1){
                 max_reach += nums[i];
                 i++;
             }
             else{
-                max_reach += max_reach+1;
+                max_reach += (max_reach+1);
                 patch++;
             }
         }
