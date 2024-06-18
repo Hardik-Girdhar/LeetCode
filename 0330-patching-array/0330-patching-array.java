9a5edc2652b4c9d79b8313411c7a609f5fcbbfbc
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-18 11:30:34 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (6.78%) - LeetHub

--- 0330-patching-array/0330-patching-array.java
+++ 0330-patching-array/0330-patching-array.java
@@ -1,34 +1,15 @@
-// class Solution {
-//     public int minPatches(int[] nums, int n) {
-//         int patch=0;
-//         long max_reach=0;
-//         int i=0;
-//         while(max_reach < n){
-//             if(i<nums.length && nums[i] <= max_reach+1){
-//                 max_reach += nums[i];
-//                 i++;
-//             }
-//             else{
-//                 max_reach += (max_reach+1);
-//                 patch++;
-//             }
-//         }
-//         return patch;
-//     }
-// }
-
 class Solution {
     public int minPatches(int[] nums, int n) {
-        int patch = 0;
+        int patch=0;
+        long max_reach=0;
         int i=0;
-        long max_reach = 0;
         while(max_reach < n){
             if(i<nums.length && nums[i] <= max_reach+1){
                 max_reach += nums[i];
                 i++;
             }
             else{
-                max_reach += max_reach+1;
+                max_reach += (max_reach+1);
                 patch++;
             }
         }
