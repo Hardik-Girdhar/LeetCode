cd47b03e771435a5bb80641632e081bf526bbd73
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-18 11:43:11 +0530

    Time: 42 ms (14.81%), Space: 60.4 MB (23.31%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -12,14 +12,29 @@
 //     }
 // }
 
+// class Solution {
+//     public int minIncrementForUnique(int[] nums) {
+//         Arrays.sort(nums);
+//         int ans=0;
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[i-1] >= nums[i]){
+//                 ans += nums[i-1]-nums[i]+1;
+//                 nums[i] = nums[i-1] + 1;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int minIncrementForUnique(int[] nums) {
         Arrays.sort(nums);
-        int ans=0;
-        for(int i=1;i<nums.length;i++){
+        int ans = 0;
+        for(int i=1; i<nums.length; i++){
             if(nums[i-1] >= nums[i]){
-                ans += nums[i-1]-nums[i]+1;
-                nums[i] = nums[i-1] + 1;
+                ans += nums[i-1] - nums[i] + 1;
+                nums[i] = nums[i-1]+1;
             }
         }
         return ans;
