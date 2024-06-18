a78db44761f3cef3b8fbd56c928dc566c3abe903
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-18 11:22:13 +0530

    Time: 6 ms (16.22%), Space: 55 MB (97.75%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,18 +1,42 @@
+// class Solution {
+//     public double findMaxAverage(int[] nums, int k) {
+//         double ans = 0;
+//         int i=0;
+//         int j=0;
+//         double sum = 0;
+//         double res=Double.NEGATIVE_INFINITY;
+//         while(j<nums.length){
+//             sum = sum + nums[j];
+//             if(j-i+1 < k){
+//                 j++;
+//             }
+//             else if(j-i+1 == k){
+//                 ans = sum/k;
+//                 res = Math.max(res,ans);
+//                 sum = sum - nums[i];
+//                 i++;
+//                 j++;
+//             }
+//         }
+//         return res;
+//     }
+// }
+
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double ans = 0;
         int i=0;
         int j=0;
-        double sum = 0;
-        double res=Double.NEGATIVE_INFINITY;
+        double ans = 0;
+        double sum=0;
+        double res = Double.NEGATIVE_INFINITY;
         while(j<nums.length){
             sum = sum + nums[j];
-            if(j-i+1 < k){
+            if((j-i+1) < k){
                 j++;
             }
-            else if(j-i+1 == k){
+            else  if(j-i+1 == k){
                 ans = sum/k;
-                res = Math.max(res,ans);
+                res = Math.max(ans,res);
                 sum = sum - nums[i];
                 i++;
                 j++;
