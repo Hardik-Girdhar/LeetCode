7307b8e8fd179634085d66c42ff6ba3976b5d6cc
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-19 13:50:41 +0530

    Time: 24 ms (40.78%), Space: 45.4 MB (78.40%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -31,43 +31,78 @@
 // //S.C : O(n)
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int n = difficulty.length;
-        int m = worker.length;
+//         int n = difficulty.length;
+//         int m = worker.length;
 
-        List<int[]> vec = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            vec.add(new int[]{difficulty[i], profit[i]});
+//         List<int[]> vec = new ArrayList<>();
+//         for (int i = 0; i < n; i++) {
+//             vec.add(new int[]{difficulty[i], profit[i]});
+//         }
+
+//         // Sort the vector based on difficulty
+//         Collections.sort(vec, (a, b) -> Integer.compare(a[0], b[0]));
+
+//         // Pre-processing to find the maximum profit till index i at constant time
+//         for (int i = 1; i < vec.size(); i++) {
+//             vec.get(i)[1] = Math.max(vec.get(i)[1], vec.get(i - 1)[1]);
+//         }
+
+//         int totalProfit = 0;
+//         for (int i = 0; i < m; i++) {
+//             int workerDiffLevel = worker[i];
+
+//             // Apply binary search on vec
+//             int l = 0, r = vec.size() - 1;
+//             int maxProfit = 0;
+//             while (l <= r) {
+//                 int mid = l + (r - l) / 2;
+//                 if (vec.get(mid)[0] <= workerDiffLevel) {
+//                     maxProfit = Math.max(maxProfit, vec.get(mid)[1]);
+//                     l = mid + 1;
+//                 } else {
+//                     r = mid - 1;
+//                 }
+//             }
+
+//             totalProfit += maxProfit;
+//         }
+
+//         return totalProfit;
+        
+        // make pair in linked list of diffculty and profit
+        List<int[]> list = new ArrayList<>();
+        for(int i=0;i<profit.length;i++){
+            list.add(new int[]{difficulty[i],profit[i]});
         }
-
-        // Sort the vector based on difficulty
-        Collections.sort(vec, (a, b) -> Integer.compare(a[0], b[0]));
-
-        // Pre-processing to find the maximum profit till index i at constant time
-        for (int i = 1; i < vec.size(); i++) {
-            vec.get(i)[1] = Math.max(vec.get(i)[1], vec.get(i - 1)[1]);
+        
+        //sort as per difficulty
+        Collections.sort(list,(a,b)->Integer.compare(a[0],b[0]));
+        
+        // take maximum profit at each array
+        for(int i=1;i<list.size()-1;i++){
+            list.get(i)[1] = Math.max(list.get(i-1)[1] , list.get(i)[1]);
         }
-
-        int totalProfit = 0;
-        for (int i = 0; i < m; i++) {
-            int workerDiffLevel = worker[i];
-
-            // Apply binary search on vec
-            int l = 0, r = vec.size() - 1;
-            int maxProfit = 0;
-            while (l <= r) {
-                int mid = l + (r - l) / 2;
-                if (vec.get(mid)[0] <= workerDiffLevel) {
-                    maxProfit = Math.max(maxProfit, vec.get(mid)[1]);
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
+        
+        int total = 0;
+        for(int i=0;i<worker.length;i++){
+            // apply binary search
+            int l=0;
+            int r=list.size()-1;
+            int max = 0;
+            while(l<=r){
+                int mid = l + (r-l)/2;
+                if(list.get(mid)[0] <= worker[i]){
+                    max = Math.max(max , list.get(mid)[1]);
+                    l = mid+1;
+                }
+                else{
+                    r=mid-1;
                 }
             }
-
-            totalProfit += maxProfit;
+            total += max;
         }
-
-        return totalProfit;
+        
+        return total;
     }
 }
 
