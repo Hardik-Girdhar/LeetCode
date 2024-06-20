ee6d065795d84a8484ec96586833dfd283f40033
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-20 11:26:42 +0530

    Time: 19 ms (46.19%), Space: 44.2 MB (85.54%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,6 +1,34 @@
+// class Solution {
+//     public int maxVowels(String s, int k) {
+//         int ans=0;
+//         int i=0;
+//         int j=0;
+//         int count=0;
+//         while(j<s.length()){
+//             if(isVowel(s.charAt(j))){
+//                 count++;
+//             }
+//             if(j-i+1 == k)
+//             {
+//                 ans = Math.max(ans,count);
+//                 if(isVowel(s.charAt(i))){
+//                     count--;
+//                 }
+//                 i++;
+//             }
+//             j++;
+//         }
+//         return ans;
+//     }
+//     public boolean isVowel(char a){
+//         a=Character.toLowerCase(a);
+//         return a=='a' || a=='e' || a=='i' || a=='o' || a=='u';
+//     }
+// }
+
 class Solution {
     public int maxVowels(String s, int k) {
-        int ans=0;
+        int ans = 0;
         int i=0;
         int j=0;
         int count=0;
@@ -8,9 +36,8 @@ class Solution {
             if(isVowel(s.charAt(j))){
                 count++;
             }
-            if(j-i+1 == k)
-            {
-                ans = Math.max(ans,count);
+            if(j-i+1 == k){
+                ans = Math.max(count,ans);
                 if(isVowel(s.charAt(i))){
                     count--;
                 }
@@ -21,37 +48,7 @@ class Solution {
         return ans;
     }
     public boolean isVowel(char a){
-        a=Character.toLowerCase(a);
+        a = Character.toLowerCase(a);
         return a=='a' || a=='e' || a=='i' || a=='o' || a=='u';
     }
 }
-
-// class Solution {
-//     public int maxVowels(String s, int k) {
-//         int ans=0;
-//         int count=0;
-//         int i=0;
-//         int j=0;
-//         while(j<s.length()){
-//             if(isVowel(s.charAt(j))){
-//                 count++;
-//             }
-//             if(j-i+1 < k){
-// //                 j++;
-// //             }
-//             if(j-i+1 == k){
-//                 ans = Math.max(count,ans);
-//                 if(isVowel(s.charAt(i))){
-//                     count--;
-//                 }
-//                 j++;
-//                 i++;
-//             }
-//         }
-//         return ans;
-//     }
-//     public static boolean isVowel(char a){
-//         a=Character.toLowerCase(a);
-//         return a=='a' || a=='e' || a=='i' || a=='o' || a=='u';
-//     }
-// }
