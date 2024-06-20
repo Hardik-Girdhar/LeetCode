35163ef81fff994961549d32fd4ab10b5f01ebd2
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-20 13:24:22 +0530

    Time: 91 ms (15.01%), Space: 57.5 MB (83.75%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,52 +1,25 @@
-// class Solution {
-//     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-//         int n=profits.length;
-//         // 1 - sort pair of element of cap and prof as per the capital 
-//         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b)->a[1]-b[1]);
-        
-//         // 2 - make pairs of prof and capital and put it in pq
-//         for(int i=0;i<n;i++){
-//             pq.add(new int[]{profits[i],capital[i]});
-//         }
-        
-//         // 3 - make maxheap priority queue on the absic of profit- which was been select on              basis of remain capital
-//         PriorityQueue<int[]> maxh = new PriorityQueue<int[]>((a,b)->b[0]-a[0]);
-        
-//         // iterate when k=0
-//         while((!pq.isEmpty() || !maxh.isEmpty()) && k>0){
-//             if(!pq.isEmpty() && pq.peek()[1] <= w){
-//                 maxh.add(pq.poll());
-//             }
-//             else{
-//                 if(!maxh.isEmpty()){
-//                     w += maxh.poll()[0];
-//                     k--;
-//                 }
-//                 else{
-//                     break;
-//                 }
-//             }
-//         }
-//         return w;
-//     }
-// }
-
-
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        PriorityQueue<int[]> maxc = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        for(int i=0;i<profits.length;i++){
-            maxc.add(new int[]{profits[i],capital[i]});
-        }
-        PriorityQueue<int[]>maxp = new PriorityQueue<>((a,b)->b[0]-a[0]);
+        int n=profits.length;
+        // 1 - sort pair of element of cap and prof as per the capital 
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b)->a[1]-b[1]);
         
-        while((!maxc.isEmpty() || !maxp.isEmpty()) && k>0){
-            if(!maxc.isEmpty() && w>=maxc.peek()[1]){
-                maxp.add(maxc.poll());
+        // 2 - make pairs of prof and capital and put it in pq
+        for(int i=0;i<n;i++){
+            pq.add(new int[]{profits[i],capital[i]});
+        }
+        
+        // 3 - make maxheap priority queue on the absic of profit- which was been select on              basis of remain capital
+        PriorityQueue<int[]> maxh = new PriorityQueue<int[]>((a,b)->b[0]-a[0]);
+        
+        // iterate when k=0
+        while((!pq.isEmpty() || !maxh.isEmpty()) && k>0){
+            if(!pq.isEmpty() && pq.peek()[1] <= w){
+                maxh.add(pq.poll());
             }
             else{
-                if(!maxp.isEmpty()){
-                    w += maxp.poll()[0];
+                if(!maxh.isEmpty()){
+                    w += maxh.poll()[0];
                     k--;
                 }
                 else{
