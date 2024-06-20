0f705437a4a54201258b5ac961e0635a1230078f
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-20 11:34:33 +0530

    Time: 20 ms (62.84%), Space: 57.7 MB (57.44%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,85 +1,84 @@
 // class Solution {
 //     public int minDays(int[] bloomDay, int m, int k) {
-//         int s = 1;
-//         int e = maxofArray(bloomDay);
-//         int ans = -1;
-//         while (s <= e) {
-//             int mid = s + (e - s) / 2;
-//             if (isPossible(bloomDay, mid, m, k)) {
+//         int ans=-1;
+//         int s=0;
+//         int e=max(bloomDay);
+//         while(s<=e){
+//             int mid = s+(e-s)/2;
+//             if(isPossible(bloomDay,mid,m,k)){
 //                 ans = mid;
-//                 e = mid - 1;
+//                 e = mid-1;
 //             }
 //             else{
-//                 s =mid + 1;
+//                 s = mid+1;
 //             }
 //         }
 //         return ans;
 //     }
-
-//     public boolean isPossible(int[] bloomDay, int mid, int nob, int nof) {
-//         int m = 0;
-//         int k = 0;
-//         for (int ele : bloomDay) {
-//             if (ele <= mid) {
-//                 k++;
-//                 if (k == nof) {
-//                     m++;
-//                     k = 0;
-//                 }
-//             } else {
-//                 k = 0;
-//             }
-//         }
-//         return m >= nob;
-//     }
-
-//     public int maxofArray(int[] arr) {
-//         int max = arr[0];
-//         for (int i : arr) {
-//             if (max < i) {
-//                 max = i;
-//             }
+//     public int max(int[] arr){
+//         int max=0;
+//         for(int i:arr){
+//             max = Math.max(max,i);
 //         }
 //         return max;
 //     }
+//     public boolean isPossible(int[] arr,int mid,int m,int k){
+//         int bouq = 0;
+//         int cons_flw = 0;
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]<=mid){
+//                 cons_flw++;
+//             }
+//             else{
+//                 cons_flw=0;
+//             }
+            
+//             if(cons_flw == k){
+//                 bouq++;
+//                 cons_flw=0;
+//             }
+//         }
+//         return bouq>=m;
+//     }
 // }
+
 
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int ans=-1;
-        int s=0;
-        int e=max(bloomDay);
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            if(isPossible(bloomDay,mid,m,k)){
+        int ans = -1;
+        int l = 0;
+        int r = max(bloomDay);
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(isPossible(bloomDay , mid , m , k)){
                 ans = mid;
-                e = mid-1;
+                r = mid-1;
             }
             else{
-                s = mid+1;
+                l=mid+1;
             }
         }
         return ans;
     }
-    public int max(int[] arr){
-        int max=0;
-        for(int i:arr){
-            max = Math.max(max,i);
+    public int max(int[] nums){
+        int max = 0;
+        for(int ele : nums){
+            max = Math.max(max , ele);
         }
         return max;
     }
-    public boolean isPossible(int[] arr,int mid,int m,int k){
+    public boolean isPossible(int[] nums, int mid, int m , int k){
         int bouq = 0;
         int cons_flw = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<=mid){
+        for(int i =0; i<nums.length;i++){
+            if(nums[i] <= mid){
                 cons_flw++;
             }
             else{
                 cons_flw=0;
             }
             
-            if(cons_flw == k){
+            if(cons_flw==k){
                 bouq++;
                 cons_flw=0;
             }
