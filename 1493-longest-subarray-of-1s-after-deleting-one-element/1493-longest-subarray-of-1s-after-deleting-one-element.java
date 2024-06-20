4731d4f129799873b7e16cbd222f24b993698188
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-20 11:19:48 +0530

    Time: 3 ms (65.80%), Space: 55.7 MB (96.06%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,44 +1,22 @@
-// // 1004. Max Consecutive Ones III  ---------- just k=1 remain same
-
-// class Solution {
-//     public int longestSubarray(int[] nums) {
-//         int k=1;
-//         int ans=0;
-//         int zeros=0;
-//         int i=0;
-//         for(int j=0;j<nums.length;j++){
-//             if(nums[j] == 0){
-//                 zeros++;
-//             }
-//             while(zeros > k){
-//                 if(nums[i]==0){
-//                     zeros--;
-//                 }
-//                 i++;
-//             }
-//             ans = Math.max(ans,j-i+1);
-//         }
-//         return ans-1;
-//     }
-// }
+// 1004. Max Consecutive Ones III  ---------- just k=1 remain same
 
 class Solution {
     public int longestSubarray(int[] nums) {
-        int zeros = 0;
-        int ans= 0;
-        int i=0;
         int k=1;
+        int ans=0;
+        int zeros=0;
+        int i=0;
         for(int j=0;j<nums.length;j++){
             if(nums[j] == 0){
                 zeros++;
             }
             while(zeros > k){
-                if(nums[i] == 0){
+                if(nums[i]==0){
                     zeros--;
                 }
                 i++;
             }
-            ans = Math.max(ans, j-i+1);
+            ans = Math.max(ans,j-i+1);
         }
         return ans-1;
     }
