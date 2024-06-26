39b518b64e8315fe95e46832408ca4cabd28a454
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-26 20:51:21 +0530

    Time: 20 ms (5.11%), Space: 59.7 MB (7.12%) - LeetHub

---
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -0,0 +1,73 @@
+// 995
+
+// class Solution {
+//     public int minOperations(int[] nums) {
+//         int k=3;
+//         int ans = 0;
+//         int flipcount=0;
+//         boolean[] isFlip = new boolean[nums.length];
+//         for(int i=0;i<nums.length;i++){
+//             if(i>=k && isFlip[i-k]==true){
+//                 flipcount--;
+//             }
+//             if(flipcount % 2== nums[i]){
+//                 if(i+k>nums.length){
+//                     return -1;
+//                 }
+//                 flipcount++;
+//                 ans++;
+//                 isFlip[i] = true;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+// class Solution {
+//     public int minOperations(int[] nums) {
+//         int k=3;
+//         int ans = 0;
+//         int flipcount = 0;
+//         for(int i=0;i<nums.length;i++){
+//             if(i>=k && nums[i-k] == 2){
+//                 flipcount--;
+//             }
+//             if(flipcount % 2 == nums[i]){
+//                 if(i+k>nums.length){
+//                     return -1;
+//                 }
+//                 flipcount++;
+//                 ans++;
+//                 nums[i]=2;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int k=3;
+        int ans=0;
+        int flipcount=0;
+        Deque<Integer> dq = new LinkedList<>();
+        for(int i=0;i<nums.length ; i++){
+            if(i>=k){
+                flipcount -= dq.pollFirst();
+            }
+            if(flipcount % 2== nums[i]){
+                if(i+k>nums.length){
+                    return -1;
+                }
+                dq.addLast(1);
+                flipcount++;
+                ans++;
+            }
+            else{
+                dq.addLast(0);
+            }
+        }   
+        return ans;
+    }
+}
