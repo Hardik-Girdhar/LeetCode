0737fd7d507c44c7905e441f4176e0dd63a5ec2c
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-26 18:06:20 +0530

    Time: 39 ms (22.34%), Space: 55.5 MB (11.63%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,15 +1,24 @@
 // class Solution {
 //     public int numberOfSubarrays(int[] nums, int k) {
-//         int n = nums.length;
-//         int[] cnt = new int[n + 1];
-//         cnt[0] = 1;
-//         int ans = 0, t = 0;
-//         for (int v : nums) {
-//             t += v & 1;
-//             if (t - k >= 0) {
-//                 ans += cnt[t - k];
+//         int ans=0;
+//         int count=0;
+//         int prev=0;
+//         int i=0;
+//         int j=0;
+//         while(j<nums.length){
+//             if(nums[j]%2 != 0 ){
+//                 count++;
+//                 prev=0;
 //             }
-//             cnt[t]++;
+//             while(count == k){
+//                 prev ++;
+//                 if(nums[i] % 2 == 1){
+//                     count--;
+//                 }
+//                 i++;
+//             }
+//             ans += prev;
+//             j++;
 //         }
 //         return ans;
 //     }
@@ -17,26 +26,15 @@
 
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int ans=0;
-        int count=0;
-        int prev=0;
-        int i=0;
-        int j=0;
-        while(j<nums.length){
-            if(nums[j]%2 != 0 ){
-                count++;
-                prev=0;
-            }
-            while(count == k){
-                prev ++;
-                if(nums[i] % 2 == 1){
-                    count--;
-                }
-                i++;
-            }
-            ans += prev;
-            j++;
+        int res = 0;
+        int curr = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int i:nums){
+            curr += i%2;      // odd - 1 , even - 0, tells no of odd
+            res += map.getOrDefault(curr-k,0);
+            map.put(curr,map.getOrDefault(curr,0)+1);
         }
-        return ans;
+        return res;
     }
 }
