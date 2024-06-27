6cbb41f528291ee9249ee82d7da361f907fefe5d
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-27 15:11:48 +0530

    Time: 0 ms (100.00%), Space: 66.7 MB (79.37%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,3 +1,19 @@
+// // TIME COMPLEXITY - O(n)
+// class Solution {
+//     public int findCenter(int[][] edges) {
+//         Map<Integer,Integer> degree = new HashMap<>();
+        
+//         for(int[] edge : edges){
+//             int u = edge[0];
+//             int v = edge[1];
+            
+//             map.put(u,map.getOrDefault(u,0)+1);
+//             map.put(v,map.getOrDefault(v,0)+1);
+//          }
+//     }
+// }
+
+
 class Solution {
     public int findCenter(int[][] edges) {
         int[] first = edges[0];
@@ -11,3 +27,18 @@ class Solution {
         }
     }
 }
+
+// TIME COMPLEXITY - CONSTANT - O(2) - O(1)
+// class Solution {
+//     public int findCenter(int[][] edges) {
+//         int[] first = edges[0];
+//         int[] second = edges[1];
+        
+//         if(first[0] == second[0] || first[0] == second[1]){
+//             return first[0];
+//         }
+//         else{
+//             return first[1];
+//         }
+//     }
+// }
