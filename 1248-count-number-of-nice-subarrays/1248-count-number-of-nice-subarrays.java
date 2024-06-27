72f0df9e5679d16ed0ae7cce35536554b874c245
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-27 16:04:30 +0530

    Time: 7 ms (69.56%), Space: 54.7 MB (53.62%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -24,17 +24,43 @@
 //     }
 // }
 
+// class Solution {
+//     public int numberOfSubarrays(int[] nums, int k) {
+//         int res = 0;
+//         int curr = 0;
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         map.put(0,1);
+//         for(int i:nums){
+//             curr += i%2;      // odd - 1 , even - 0, tells no of odd
+//             res += map.getOrDefault(curr-k,0);
+//             map.put(curr,map.getOrDefault(curr,0)+1);
+//         }
+//         return res;
+//     }
+// }
+
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int res = 0;
-        int curr = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0,1);
-        for(int i:nums){
-            curr += i%2;      // odd - 1 , even - 0, tells no of odd
-            res += map.getOrDefault(curr-k,0);
-            map.put(curr,map.getOrDefault(curr,0)+1);
+        int ans =0;
+        int count=0;
+        int prev=0;
+        int i=0;
+        int j=0;
+        while(j<nums.length){
+            if(nums[j] % 2 != 0){
+                count++;
+                prev = 0;
+            }
+            while(count == k){
+                prev++;
+                if(nums[i] % 2 == 1){
+                    count--;
+                }
+                i++;
+            }
+            ans += prev;
+            j++;
         }
-        return res;
+        return ans;
     }
 }
