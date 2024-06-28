6a84bd5018eb6a093e38b355d0292bd3e3680beb
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-28 13:51:16 +0530

    Time: 10 ms (79.70%), Space: 44.9 MB (98.89%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,29 +1,61 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if(word1.length() != word2.length()){
-            return false;
-        }
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
-        
-        for(int i=0;i<word1.length();i++){
-            freq1[word1.charAt(i)-'a']++;
-            freq2[word2.charAt(i)-'a']++;
+
+        for (char ch : word1.toCharArray()) {
+            freq1[ch - 'a']++;
         }
-        
-        for(int i=0; i<26;i++){
-            if(freq1[i] == 0 && freq2[i] == 0) continue;
-            if(freq1[i] != 0 && freq2[i] !=0 ) continue;
-            return false;
+
+        for (char ch : word2.toCharArray()) {
+            freq2[ch - 'a']++;
         }
-        
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
         Arrays.sort(freq1);
         Arrays.sort(freq2);
-        
-        for(int i=0;i<26;i++){
-            if(freq1[i] == freq2[i]) continue;
-            return false;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
         }
+
         return true;
     }
 }
+
+// class Solution {
+//     public boolean closeStrings(String word1, String word2) {
+//         if(word1.length() != word2.length()){
+//             return false;
+//         }
+//         int[] freq1 = new int[26];
+//         int[] freq2 = new int[26];
+        
+//         for(int i=0;i<word1.length();i++){
+//             freq1[word1.charAt(i)-'a']++;
+//             freq2[word2.charAt(i)-'a']++;
+//         }
+        
+//         for(int i=0; i<26;i++){
+//             if(freq1[i] == 0 && freq2[i] == 0) continue;
+//             if(freq1[i] != 0 && freq2[i] !=0 ) continue;
+//             return false;
+//         }
+        
+//         Arrays.sort(freq1);
+//         Arrays.sort(freq2);
+        
+//         for(int i=0;i<26;i++){
+//             if(freq1[i] == freq2[i]) continue;
+//             return false;
+//         }
+//         return true;
+//     }
+// }
