896f3865b3b30a644a371dd257d956d4fb6746ec
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-28 14:21:24 +0530

    Time: 1 ms (87.61%), Space: 42.1 MB (10.80%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,30 +1,58 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if(allcapital(word) || allsmall(word) || allsmall(word.substring(1))){
+        int countcapital = 0;
+        for(int i=0;i<word.length() ;i++){
+            char ch=word.charAt(i);
+            
+            if(ch>='A' && ch<='Z'){
+                countcapital++;
+            }
+        }
+        
+        if(countcapital == 0){
             return true;
         }
-        else{
-            return false;
+        
+        if(countcapital == word.length()){
+            return true;
         }
-    }
-    
-    public boolean allcapital(String word){
-        for(int i=0;i<word.length() ;i++){
-            char ch = word.charAt(i);
-            if(ch <'A' || ch>'Z'){
-                return false;
-            }
+        
+        if(countcapital == 1 && (word.charAt(0)>='A' && word.charAt(0)<='Z')){
+            return true;
         }
-        return true;
-    }
-    
-    public boolean allsmall(String word){
-        for(int i=0;i<word.length() ;i++){
-            char ch = word.charAt(i);
-            if(ch<'a' || ch>'z'){
-                return false;
-            }
-        }
-        return true;
+           
+        return false;
     }
 }
+
+
+// class Solution {
+//     public boolean detectCapitalUse(String word) {
+//         if(allcapital(word) || allsmall(word) || allsmall(word.substring(1))){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+    
+//     public boolean allcapital(String word){
+//         for(int i=0;i<word.length() ;i++){
+//             char ch = word.charAt(i);
+//             if(ch <'A' || ch>'Z'){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+    
+//     public boolean allsmall(String word){
+//         for(int i=0;i<word.length() ;i++){
+//             char ch = word.charAt(i);
+//             if(ch<'a' || ch>'z'){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
