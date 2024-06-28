1fe86c83169700205eaeb7d0fda0df15edc614e9
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-06-28 13:33:55 +0530

    Time: 2 ms (96.32%), Space: 41.2 MB (97.64%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,13 +1,29 @@
+// class Solution {
+//     public boolean uniqueOccurrences(int[] arr) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int i=0;i<arr.length; i++){
+//             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+//         }
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int ele:map.values()){
+//             set.add(ele);
+//         }
+//         return map.size() == set.size();
+//     }
+// }
+
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length; i++){
+        HashMap<Integer ,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
+        
         HashSet<Integer> set = new HashSet<>();
         for(int ele:map.values()){
             set.add(ele);
         }
-        return map.size() == set.size();
+        
+        return set.size() == map.size();
     }
 }
