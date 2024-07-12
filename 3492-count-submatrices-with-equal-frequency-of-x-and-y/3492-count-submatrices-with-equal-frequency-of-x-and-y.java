4a005b49044571501037312ae8d917d6614ce567
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-13 00:46:06 +0530

    Time: 19 ms (93.09%), Space: 130.8 MB (57.71%) - LeetHub

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/3492-count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/3492-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,19 +1,68 @@
+// class Solution {
+//     public int numberOfSubmatrices(char[][] grid) {
+//         int ans=0;
+//         int[] countX = new int[grid[0].length];
+//         int[] countY = new int[grid[0].length];
+//         for(int i=0; i<grid.length; i++){
+//             int x=0;
+//             int y=0;
+//             for(int j=0;j<grid[i].length;j++){
+//                 if(grid[i][j] == 'X') x++;
+//                 if(grid[i][j] == 'Y') y++;
+//                 countX[j] += x;
+//                 countY[j] += y;
+//                 if(countX[j] >= 1 && countY[j] == countX[j]) ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
 class Solution {
+    // public boolean isValid(char[][] grid, int strow, int stcol, int endrow, int endcol){
+    //     int cntX=0;
+    //     int cntY=0;
+    //     boolean containsX=false;
+    //     for(int i=strow; i<=endrow;i++){
+    //         for(int j=stcol;j<=endcol;j++){
+    //             if(grid[i][j]=='X'){
+    //                 cntX++;
+    //                 containsX=true;
+    //             }else if(grid[i][j]=='Y'){
+    //                 cntY++;
+    //             }
+    //         }
+    //     }
+    //     return containsX && cntX==cntY;
+    // }
+    
+    
     public int numberOfSubmatrices(char[][] grid) {
-        int ans=0;
-        int[] countX = new int[grid[0].length];
-        int[] countY = new int[grid[0].length];
-        for(int i=0; i<grid.length; i++){
-            int x=0;
-            int y=0;
-            for(int j=0;j<grid[i].length;j++){
-                if(grid[i][j] == 'X') x++;
-                if(grid[i][j] == 'Y') y++;
-                countX[j] += x;
-                countY[j] += y;
-                if(countX[j] >= 1 && countY[j] == countX[j]) ans++;
+        // Brute force - throws tle
+        // int cnt=0;
+        // for(int i=0;i<grid.length;i++){
+        //     for(int j=0;j<grid[0].length;j++){
+        //         if(isValid(grid,0,0,i,j)) cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        // Optimal - tc: O(mn) sc: O(nm)
+        int n=grid.length;
+        int m=grid[0].length;
+        int cnt=0;
+        int[][] x=new int[n+1][m+1];
+        int[][] y=new int[n+1][m+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                x[i+1][j+1]=x[i+1][j]+x[i][j+1]-x[i][j] + (grid[i][j]=='X' ? 1:0);
+                y[i+1][j+1]=y[i+1][j]+y[i][j+1]-y[i][j] + (grid[i][j]=='Y' ? 1:0);
+
+                if(x[i+1][j+1]==y[i+1][j+1] && x[i+1][j+1]>0) cnt++;
             }
+
         }
-        return ans;
+        return cnt;
     }
 }
