679348c09821ed2ec93d5e8b8b09ee1f37e441ca
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-12 18:37:39 +0530

    Time: 3 ms (78.72%), Space: 45.3 MB (99.32%) - LeetHub

--- 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.java
@@ -1,21 +1,51 @@
+// class Solution {
+//     public List<String> validStrings(int n) {
+//         List<String> list = new ArrayList<>();
+//         list.add("1");
+//         list.add("0");
+//         if(n==1){
+//             return list;
+//         }
+//         for(int i=2;i<=n;i++){
+//             List<String> temp = new ArrayList<>();
+//             for(String s : list){
+//                 temp.add(s+"1");
+//                 if(s.charAt(s.length()-1) == '1'){
+//                     temp.add(s+"0");
+//                 }
+//             }
+//             list = temp;
+//         }
+//         return list;
+//     }
+// }
+
 class Solution {
     public List<String> validStrings(int n) {
         List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("0");
-        if(n==1){
-            return list;
-        }
-        for(int i=2;i<=n;i++){
-            List<String> temp = new ArrayList<>();
-            for(String s : list){
-                temp.add(s+"1");
-                if(s.charAt(s.length()-1) == '1'){
-                    temp.add(s+"0");
-                }
-            }
-            list = temp;
-        }
+        String s = "";
+        findBinary(0,n,list,s);
         return list;
+    }
+
+    private void findBinary(int start, int n, List<String>list, String s){
+        for(int i=start ; i<=n; i++){
+            //Base Condition
+            if(s.length() == n){
+                list.add(s);
+                return;
+            }
+
+            if(s.length()>0 && s.charAt(s.length()-1) == '0'){
+                findBinary(start+1,n,list,s+"1");
+                return ;
+            }
+
+            else{
+                findBinary(start+1,n,list,s+"1");
+                findBinary(start+1,n,list,s+"0");
+                return;
+            }
+        }
     }
 }
