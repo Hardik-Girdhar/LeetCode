be66a9bce1ec314e339b9ddf50435d53695788d2
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-13 00:31:08 +0530

    Time: 17 ms (98.94%), Space: 127.3 MB (75.08%) - LeetHub

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/3492-count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/3492-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,17 +1,39 @@
+// class Solution {
+//     public int numberOfSubmatrices(char[][] grid) {
+//         int ans=0;
+//         int[] countX = new int[grid[0].length];
+//         int[] countY = new int[grid[0].length];
+//         for(int i=0; i<grid.length; i++){
+//             int x=0;
+//             int y=0;
+//             for(int j=0;j<grid[i].length;j++){
+//                 if(grid[i][j] == 'X') x++;
+//                 if(grid[i][j] == 'Y') y++;
+//                 countX[j] += x;
+//                 countY[j] += y;
+//                 if(countX[j] >= 1 && countY[j] == countX[j]) ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int numberOfSubmatrices(char[][] grid) {
-        int ans=0;
-        int[] countX = new int[grid[0].length];
-        int[] countY = new int[grid[0].length];
-        for(int i=0; i<grid.length; i++){
-            int x=0;
-            int y=0;
+        int ans = 0;
+        int countX[] = new int[grid[0].length];
+        int countY[] = new int[grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            int x = 0;
+            int y = 0;
             for(int j=0;j<grid[i].length;j++){
                 if(grid[i][j] == 'X') x++;
                 if(grid[i][j] == 'Y') y++;
+
                 countX[j] += x;
                 countY[j] += y;
-                if(countX[j] >= 1 && countY[j] == countX[j]) ans++;
+
+                if(countX[j] >= 1 && countX[j] == countY[j]) ans++;
             }
         }
         return ans;
