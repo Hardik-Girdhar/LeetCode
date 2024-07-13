a72187360170bfd6c9d790972cb5d2f7ce4ea9fb
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-13 16:44:56 +0530

    Time: 3 ms (29.29%), Space: 42.1 MB (5.86%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,73 +1,32 @@
-// class Solution {
-//     public String decodeString(String s) {
-//         Stack<Character> st = new Stack<>();
-//         for(int i=0;i<s.length();i++){
-//             if(s.charAt(i) == ']'){
-//                 StringBuilder sb = new StringBuilder();
-//                 while(!st.isEmpty() && st.peek()!='['){
-//                     sb.insert(0,st.pop());
-//                 }
-//                 st.pop();
-//                 StringBuilder nums = new StringBuilder();
-//                 while(!st.isEmpty() && Character.isDigit(st.peek())){
-//                     nums.insert(0,st.pop());
-//                 }
-//                 int number = Integer.parseInt(nums.toString());
-//                 StringBuilder repeat = new StringBuilder();
-//                 for(int j=0;j<number;j++){
-//                     repeat.append(sb);
-//                 }
-//                 for(int k=0;k<repeat.length();k++){
-//                     st.push(repeat.charAt(k));
-//                 }
-                
-//             }
-//             else{
-//                 st.push(s.charAt(i));
-//             }
-//         }
-//         StringBuilder res= new StringBuilder();
-//         while(!st.isEmpty()){
-//             res.insert(0,st.pop());
-//         }
-//         return res.toString();
-//     }
-// }
-
-
 class Solution {
     public String decodeString(String s) {
         Stack<Character> st = new Stack<>();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == ']'){
-                StringBuilder ch = new StringBuilder();
+                StringBuilder sb = new StringBuilder();
                 while(!st.isEmpty() && st.peek()!='['){
-                    ch.insert(0,st.pop());
+                    sb.insert(0,st.pop());
                 }
                 st.pop();
-
-                StringBuilder number = new StringBuilder();
+                StringBuilder nums = new StringBuilder();
                 while(!st.isEmpty() && Character.isDigit(st.peek())){
-                    number.insert(0,st.pop());
+                    nums.insert(0,st.pop());
                 }
-
-                int nums = Integer.parseInt(number.toString());
-
+                int number = Integer.parseInt(nums.toString());
                 StringBuilder repeat = new StringBuilder();
-                for(int j=0;j<nums;j++){
-                    repeat.append(ch);
+                for(int j=0;j<number;j++){
+                    repeat.append(sb);
                 }
-
-                for(int j=0;j<repeat.length() ; j++){
-                    st.push(repeat.charAt(j));
+                for(int k=0;k<repeat.length();k++){
+                    st.push(repeat.charAt(k));
                 }
+                
             }
             else{
                 st.push(s.charAt(i));
             }
         }
-
-        StringBuilder res = new StringBuilder();
+        StringBuilder res= new StringBuilder();
         while(!st.isEmpty()){
             res.insert(0,st.pop());
         }
