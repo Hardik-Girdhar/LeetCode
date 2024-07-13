fc4ea1d60865c08cec0e10bce922cd717e80309c
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-13 12:53:32 +0530

    Time: 39 ms (65%), Space: 60.9 MB (56.25%) - LeetHub

--- 2846-robot-collisions/2846-robot-collisions.java
+++ 2846-robot-collisions/2846-robot-collisions.java
@@ -1,3 +1,54 @@
+// class Solution {
+//     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+//         List<Integer> list = new ArrayList<>();
+//         for(int i=0;i<positions.length;i++){
+//             list.add(i);
+//         }
+
+//         Collections.sort(list,(a,b)->Integer.compare(positions[a],positions[b]));
+
+//         Stack<Integer> st = new Stack<>();
+//         for(var l : list){
+//             if(directions.charAt(l) == 'L'){
+//                 while(!st.isEmpty()){
+//                     int temp = st.peek();
+//                     if(healths[temp] == healths[l]){
+//                         healths[temp] = 0;
+//                         healths[l] = 0;
+//                         st.pop();
+//                         break;
+//                     }
+
+//                     else if(healths[l] > healths[temp]){
+//                         healths[l]--;
+//                         healths[temp] = 0;
+//                         st.pop();
+//                     }
+
+//                     else{
+//                         healths[temp]--;
+//                         healths[l] = 0;
+//                         break;
+//                     }
+//                 }
+//             }
+//             else{
+//                 st.push(l);
+//             }
+//         }
+
+//         List<Integer> res = new ArrayList<>();
+//         for(int h : healths){
+//             if(h!=0){
+//                 res.add(h);
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+
+
 class Solution {
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
         List<Integer> list = new ArrayList<>();
@@ -8,26 +59,26 @@ class Solution {
         Collections.sort(list,(a,b)->Integer.compare(positions[a],positions[b]));
 
         Stack<Integer> st = new Stack<>();
-        for(var l : list){
+        for(int l : list){
             if(directions.charAt(l) == 'L'){
                 while(!st.isEmpty()){
                     int temp = st.peek();
-                    if(healths[temp] == healths[l]){
-                        healths[temp] = 0;
-                        healths[l] = 0;
+                    if(healths[l] == healths[temp]){
+                        healths[l]=0;
+                        healths[temp]=0;
                         st.pop();
                         break;
                     }
 
                     else if(healths[l] > healths[temp]){
-                        healths[l]--;
                         healths[temp] = 0;
+                        healths[l]--;
                         st.pop();
                     }
 
                     else{
+                        healths[l]=0;
                         healths[temp]--;
-                        healths[l] = 0;
                         break;
                     }
                 }
@@ -37,12 +88,12 @@ class Solution {
             }
         }
 
-        List<Integer> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         for(int h : healths){
             if(h!=0){
-                res.add(h);
+                ans.add(h);
             }
         }
-        return res;
+        return ans;
     }
 }
