c779adf9195ef6ae7d33c94c7ccf93091c4b8b01
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-14 21:27:32 +0530

    Time: 5 ms (57.69%), Space: 41.9 MB (36.43%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.java
+++ 0726-number-of-atoms/0726-number-of-atoms.java
@@ -1,7 +1,76 @@
+// class Solution {
+//     public String countOfAtoms(String formula) {
+//         int n = formula.length();
+
+//         Stack<Map<String,Integer>> st = new Stack<>();
+//         st.push(new HashMap<>());
+
+//         int i=0;
+//         while(i<n){
+//             if(formula.charAt(i) == '('){
+//                 st.push(new HashMap<>());
+//                 i++;
+//             }
+//             else if(formula.charAt(i) == ')'){
+//                 Map<String,Integer> curr = st.pop();
+//                 i++;
+
+//                 StringBuilder sb = new StringBuilder();
+//                 while(i<n && Character.isDigit(formula.charAt(i))){
+//                     sb.append(formula.charAt(i));
+//                     i++;
+//                 } 
+
+//                 int multiplier = sb.length() > 0 ? Integer.parseInt(sb.toString()) : 1;
+//                 for(String key : curr.keySet()){
+//                     int value = curr.get(key);
+//                     curr.put(key,value * multiplier);
+//                 }
+
+//                 for(String key : curr.keySet()){
+//                     st.peek().put(key, st.peek().getOrDefault(key, 0) + curr.get(key));
+//                 }
+//             }
+//             else{
+//                 StringBuilder ele = new StringBuilder();
+//                 ele.append(formula.charAt(i));
+//                 i++;
+
+//                 while(i<n && Character.isLowerCase(formula.charAt(i))){
+//                     ele.append(formula.charAt(i));
+//                     i++;
+//                 }
+
+//                 StringBuilder sb = new StringBuilder();
+//                 while(i<n && Character.isDigit(formula.charAt(i))){
+//                     sb.append(formula.charAt(i));
+//                     i++;
+//                 }
+
+//                 int count = sb.length()>0 ? Integer.parseInt(sb.toString()) : 1;
+
+//                 st.peek().put(ele.toString() , st.peek().getOrDefault(ele.toString(),0)+count);
+//             }
+//         }
+
+//         StringBuilder res = new StringBuilder();
+//         TreeMap<String,Integer> sortedMap = new TreeMap<>(st.peek());
+//         for(String key : sortedMap.keySet()){
+//             res.append(key);
+//             int count = sortedMap.get(key);
+//             if(count>1){
+//                 res.append(count);
+//             }
+//         } 
+
+//         return res.toString();
+//     }
+// }
+
+
 class Solution {
     public String countOfAtoms(String formula) {
-        int n = formula.length();
-
+        int n=formula.length();
         Stack<Map<String,Integer>> st = new Stack<>();
         st.push(new HashMap<>());
 
@@ -12,57 +81,53 @@ class Solution {
                 i++;
             }
             else if(formula.charAt(i) == ')'){
-                Map<String,Integer> curr = st.pop();
+                Map<String, Integer> curr = st.pop();
                 i++;
 
                 StringBuilder sb = new StringBuilder();
                 while(i<n && Character.isDigit(formula.charAt(i))){
                     sb.append(formula.charAt(i));
                     i++;
-                } 
+                }
 
-                int multiplier = sb.length() > 0 ? Integer.parseInt(sb.toString()) : 1;
+                int number = sb.length() > 0 ? Integer.parseInt(sb.toString()) : 1;
                 for(String key : curr.keySet()){
                     int value = curr.get(key);
-                    curr.put(key,value * multiplier);
+                    curr.put(key, value*number);
                 }
 
                 for(String key : curr.keySet()){
-                    st.peek().put(key, st.peek().getOrDefault(key, 0) + curr.get(key));
+                    st.peek().put(key,st.peek().getOrDefault(key,0)+curr.get(key));
                 }
             }
             else{
                 StringBuilder ele = new StringBuilder();
-                ele.append(formula.charAt(i));
-                i++;
+                ele.append(formula.charAt(i++));
 
                 while(i<n && Character.isLowerCase(formula.charAt(i))){
-                    ele.append(formula.charAt(i));
-                    i++;
+                    ele.append(formula.charAt(i++));
                 }
 
                 StringBuilder sb = new StringBuilder();
                 while(i<n && Character.isDigit(formula.charAt(i))){
-                    sb.append(formula.charAt(i));
-                    i++;
+                    sb.append(formula.charAt(i++));
                 }
 
-                int count = sb.length()>0 ? Integer.parseInt(sb.toString()) : 1;
-
-                st.peek().put(ele.toString() , st.peek().getOrDefault(ele.toString(),0)+count);
+                int count = sb.length() > 0 ? Integer.parseInt(sb.toString()) : 1;
+                st.peek().put(ele.toString(),st.peek().getOrDefault(ele.toString(),0)+count);
             }
         }
 
         StringBuilder res = new StringBuilder();
-        TreeMap<String,Integer> sortedMap = new TreeMap<>(st.peek());
-        for(String key : sortedMap.keySet()){
-            res.append(key);
-            int count = sortedMap.get(key);
-            if(count>1){
-                res.append(count);
-            }
-        } 
+        TreeMap<String,Integer> tree = new TreeMap<>(st.peek());
 
+        for(String key : tree.keySet()){
+            res.append(key);
+            int value = tree.get(key);
+            if(value>1){
+                res.append(value);
+            }
+        }
         return res.toString();
     }
 }
