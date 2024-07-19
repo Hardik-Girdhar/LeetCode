83d32a53da3fab6030be7900b5af265b00089e84
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-19 22:10:26 +0530

    Time: 0 ms (100%), Space: 44.7 MB (6.51%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -1,20 +1,34 @@
+// //BRUTE FORCE
+// class Solution {
+//     public int diagonalSum(int[][] matrix) {
+//         int row = matrix.length;
+//         int col = matrix[0].length;
+//         int sum = 0;
+
+//         for(int i=0; i<row; i++){
+//             for(int j =0;j<col;j++){
+//                 if(i==j){
+//                     sum += matrix[i][j];
+//                 }
+//                 else if(i+j == row-1){
+//                     sum += matrix[i][j];
+//                 }
+//             }
+//         }
+//         return sum;
+//     }
+// }
+
 class Solution {
     public int diagonalSum(int[][] matrix) {
         int row = matrix.length;
-        int col = matrix[0].length;
         int sum = 0;
-
-        for(int i=0; i<row; i++){
-            for(int j =0;j<col;j++){
-                if(i==j){
-                    sum += matrix[i][j];
-                }
-                else if(i+j == row-1){
-                    sum += matrix[i][j];
-                }
+        for (int i = 0; i < row; i++) {
+            sum += matrix[i][i];
+            if (i != row - i - 1) {
+                sum += matrix[i][row - i - 1];
             }
         }
-
         return sum;
     }
 }
