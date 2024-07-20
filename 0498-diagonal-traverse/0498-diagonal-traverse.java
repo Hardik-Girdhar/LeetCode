de25a1c275cd6ab875105c9efac5e5d22a5f72bc
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-20 20:07:25 +0530

    Time: 3 ms (67.07%), Space: 47.1 MB (50.59%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,17 +1,61 @@
+// class Solution {
+//     public int[] findDiagonalOrder(int[][] matrix) {
+//         int m=matrix.length;
+//         int n=matrix[0].length;
+//         if(m==0 || n==0){
+//             return new int[0];
+//         }
+
+//         int[] arr = new int[m*n];
+//         int i=0;
+//         int row=0,col=0;
+//         boolean up = true;
+//         while(row<m && col<n){
+//             // up arrow
+//             if(up){
+//                 while(row>0 && col<n-1){
+//                     arr[i++] = matrix[row][col];
+//                     row--;
+//                     col++;
+//                 }
+//                 arr[i++] = matrix[row][col];
+//                 if(col == n-1){
+//                     row++;
+//                 }
+//                 else{
+//                     col++;
+//                 }
+//             }
+//             else{
+//                 while(col>0 && row<m-1){
+//                     arr[i++] = matrix[row][col];
+//                     col--;
+//                     row++;
+//                 }
+//                 arr[i++] = matrix[row][col];
+//                 if(row == m-1){
+//                     col++;
+//                 }
+//                 else{
+//                     row++;
+//                 }
+//             }
+//             // down arrow
+//             up = !up;
+//         }
+//         return arr;
+//     }
+// }
+
 class Solution {
     public int[] findDiagonalOrder(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        if(m==0 || n==0){
-            return new int[0];
-        }
-
-        int[] arr = new int[m*n];
         int i=0;
         int row=0,col=0;
-        boolean up = true;
+        int[] arr = new int[m*n]; 
+        boolean up=true;
         while(row<m && col<n){
-            // up arrow
             if(up){
                 while(row>0 && col<n-1){
                     arr[i++] = matrix[row][col];
@@ -21,13 +65,12 @@ class Solution {
                 arr[i++] = matrix[row][col];
                 if(col == n-1){
                     row++;
-                }
-                else{
+                }else{
                     col++;
                 }
             }
             else{
-                while(col>0 && row<m-1){
+                while(col>0 && row < m-1){
                     arr[i++] = matrix[row][col];
                     col--;
                     row++;
@@ -35,12 +78,10 @@ class Solution {
                 arr[i++] = matrix[row][col];
                 if(row == m-1){
                     col++;
-                }
-                else{
+                }else{
                     row++;
                 }
             }
-            // down arrow
             up = !up;
         }
         return arr;
