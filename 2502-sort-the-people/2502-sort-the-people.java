8a2fbe96693bbb72b1f9f953d20996b9e775ff73
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-22 20:17:20 +0530

    Time: 7 ms (90.64%), Space: 45.1 MB (82.91%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,16 +1,27 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-       HashMap<Integer,String> map = new HashMap<Integer,String>();
-       for(int i = 0;i<names.length;i++){
-        map.put(heights[i],names[i]);
-       }
-       Arrays.sort(heights);
-       String res[] = new String[heights.length];
-       int j =0;
-       for(int i = heights.length-1;i>=0;i--){
-        res[j] = map.get(heights[i]);
-        j++;
-       }
-       return res;
+        // HashMap<Integer,String> map = new HashMap<Integer,String>();
+        // for(int i = 0;i<names.length;i++){
+        // map.put(heights[i],names[i]);
+        // }
+        // Arrays.sort(heights);
+        // String res[] = new String[heights.length];
+        // int j =0;
+        // for(int i = heights.length-1;i>=0;i--){
+        // res[j] = map.get(heights[i]);
+        // j++;
+        // }
+        // return res;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        for (int i = 0; i < heights.length; i++) {
+            pq.add(new int[] { i, heights[i] });
+        }
+        String[] res = new String[names.length];
+        int i = 0;
+        while (!pq.isEmpty()) {
+            res[i++] = names[pq.poll()[0]];
+        }
+        return res;
     }
 }
