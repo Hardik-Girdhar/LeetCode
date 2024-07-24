25a3043bb82605eea40db6741a251693938d9490
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-24 23:50:42 +0530

    Time: 216 ms (52.94%), Space: 55.4 MB (59.8%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.java
@@ -33,37 +33,69 @@
 // }
 
 
+// class Solution {
+//     public int[] sortJumbled(int[] mapping, int[] nums) {
+//         int n = nums.length;
+//         int[][] mapped = new int[n][2];
+        
+//         for (int i = 0; i < n; i++) {
+//             int original = nums[i];
+//             StringBuilder sb = new StringBuilder();
+            
+//             if (original == 0) {
+//                 sb.append(mapping[0]);
+//             } else {
+//                 int num = original;
+//                 while (num != 0) {
+//                     int r = num % 10;
+//                     num /= 10;
+//                     sb.insert(0, mapping[r]);
+//                 }
+//             }
+            
+//             mapped[i][0] = original;
+//             mapped[i][1] = Integer.parseInt(sb.toString());
+//         }
+
+//         Arrays.sort(mapped, (a, b) -> a[1] - b[1]);
+        
+//         int[] res = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             res[i] = mapped[i][0];
+//         }
+        
+//         return res;
+//     }
+// }
+
+
+
 class Solution {
     public int[] sortJumbled(int[] mapping, int[] nums) {
-        int n = nums.length;
-        int[][] mapped = new int[n][2];
-        
-        for (int i = 0; i < n; i++) {
-            int original = nums[i];
+        int[][] map = new int[nums.length][2];
+        for(int i=0;i<nums.length;i++){
             StringBuilder sb = new StringBuilder();
-            
-            if (original == 0) {
+            int num = nums[i];
+            if(num == 0){
                 sb.append(mapping[0]);
-            } else {
-                int num = original;
-                while (num != 0) {
-                    int r = num % 10;
-                    num /= 10;
-                    sb.insert(0, mapping[r]);
+            }
+            else{
+                while(num != 0){
+                    int r = num%10;
+                    num = num/10;
+                    sb.insert(0,mapping[r]);
                 }
             }
-            
-            mapped[i][0] = original;
-            mapped[i][1] = Integer.parseInt(sb.toString());
+            map[i][0] = nums[i];
+            map[i][1] = Integer.parseInt(sb.toString());
         }
 
-        Arrays.sort(mapped, (a, b) -> a[1] - b[1]);
-        
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            res[i] = mapped[i][0];
+        Arrays.sort(map,(a,b)-> a[1]-b[1]) ;
+
+        int[] res = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            res[i] = map[i][0];
         }
-        
         return res;
     }
 }
