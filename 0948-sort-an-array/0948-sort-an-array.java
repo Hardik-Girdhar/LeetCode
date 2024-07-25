2a52eb680b4d6dee5a8f797544db68a8d40ba376
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-07-25 20:19:52 +0530

    Time: 28 ms (50.43%), Space: 57 MB (17.02%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,25 +1,74 @@
+// COUNTING SORT - O(n+k)
+// class Solution {
+//     public int[] sortArray(int[] nums) {
+//         Map<Integer,Integer> map = new HashMap<>();
+//         for(int num : nums){
+//             map.put(num,map.getOrDefault(num,0)+1);
+//         }
+
+//         int min = Integer.MAX_VALUE;
+//         int max = Integer.MIN_VALUE;
+//         for(int num : nums){
+//             min = Math.min(min,num);
+//             max = Math.max(max,num);
+//         }
+
+//         int idx=0;
+//         for(int i=min;i<=max;i++){
+//             while(map.getOrDefault(i,0)>0){
+//                 nums[idx] = i;
+//                 idx++;
+//                 map.put(i,map.get(i)-1);
+//             }
+//         }
+//         return nums;
+//     }
+// }
+
+
+// MERGE SORT - O(nlogn)
 class Solution {
     public int[] sortArray(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+        int s = 0;
+        int e = nums.length-1;
+        mergeSort(nums, s, e);
+        return nums;
+    }
+
+    private void mergeSort(int[] arr, int s, int e){
+        if(s == e){
+            return;
         }
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for(int num : nums){
-            min = Math.min(min,num);
-            max = Math.max(max,num);
-        }
+        int mid = s + (e-s)/2;
+        mergeSort(arr, s, mid);
+        mergeSort(arr, mid+1, e);
+        merge(arr, s, mid, e);
+    }
 
-        int idx=0;
-        for(int i=min;i<=max;i++){
-            while(map.getOrDefault(i,0)>0){
-                nums[idx] = i;
-                idx++;
-                map.put(i,map.get(i)-1);
+    private void merge(int[]arr, int s, int mid, int e){
+        int[] temp = new int[e-s+1];
+        int i=s;
+        int j=mid+1;
+        int k=0;
+        while(i<=mid && j<=e){
+            if(arr[i] < arr[j]){
+                temp[k++] = arr[i++];
+            }
+            else{
+                temp[k++] = arr[j++];
             }
         }
-        return nums;
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+        while(j<=e){
+            temp[k++] = arr[j++];
+        }
+
+        int m=s;
+        for(int ele:temp){
+            arr[m++] = ele;
+        }
     }
 }
