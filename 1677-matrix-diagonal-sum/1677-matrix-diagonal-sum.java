a0ee231cccb40f7638e1b52c4f86121983d4253f
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-08-23 23:09:02 +0530

    Time: 0 ms (100%), Space: 44.3 MB (78.39%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -1,19 +1,35 @@
-// Brute force
+// optimized
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int n = mat.length;
-        int primarySum=0;
-        int secondarySum=0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i == j){
-                    primarySum += mat[i][j];
-                }
-                else if(i+j == n-1){
-                    secondarySum += mat[i][j];
-                }
+        int n=mat.length;
+        int primarySum = 0;
+        int secondarySum = 0;
+        for(int i=0;i<n;i++){
+            primarySum += mat[i][i];
+            if(i != n-1-i){
+                secondarySum += mat[i][n-1-i];
             }
         }
         return primarySum + secondarySum;
     }
 }
+
+// // Brute force
+// class Solution {
+//     public int diagonalSum(int[][] mat) {
+//         int n = mat.length;
+//         int primarySum=0;
+//         int secondarySum=0;
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 if(i == j){
+//                     primarySum += mat[i][j];
+//                 }
+//                 else if(i+j == n-1){
+//                     secondarySum += mat[i][j];
+//                 }
+//             }
+//         }
+//         return primarySum + secondarySum;
+//     }
+// }
