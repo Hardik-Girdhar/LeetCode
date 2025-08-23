8c9dc02d043691ddd0fb37d5975c273e475439fc
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-08-23 22:01:43 +0530

    Time: 0 ms (100%), Space: 41.8 MB (26.37%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,40 +1,41 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> list = new ArrayList<>();
-        int startRow = 0;
-        int endRow = matrix.length - 1;
-        int startCol = 0;
-        int endCol = matrix[0].length - 1;
 
-        while (startRow <= endRow && startCol <= endCol) {
+        int sRow = 0;
+        int eRow = matrix.length-1;
+        int sCol = 0;
+        int eCol = matrix[0].length-1;
+
+        while(sRow <= eRow && sCol <= eCol){
             // top
-            for (int j = startCol; j <= endCol; j++) {
-                list.add(matrix[startRow][j]);
+            for(int j=sCol; j<=eCol; j++){
+                list.add(matrix[sRow][j]);
             }
-            startRow++;
 
             // right
-            for (int i = startRow; i <= endRow; i++) {
-                list.add(matrix[i][endCol]);
+            for(int i=sRow+1; i<=eRow; i++){
+                list.add(matrix[i][eCol]);
             }
-            endCol--;
 
             // bottom
-            if (startRow <= endRow) {
-                for (int j = endCol; j >= startCol; j--) {
-                    list.add(matrix[endRow][j]);
-                }
+            for(int j=eCol-1; j>=sCol; j--){
+                if(sRow == eRow) break;           // single row -- has already covered above
+                list.add(matrix[eRow][j]);
             }
-            endRow--;
 
             // left
-            if (startCol <= endCol) {
-                for (int i = endRow; i >= startRow; i--) {
-                    list.add(matrix[i][startCol]);
-                }
+            for(int i=eRow-1; i>sRow; i--){
+                if(sCol == eCol) break;         // single column -- has already covered above
+                list.add(matrix[i][sCol]);
             }
-            startCol++;
+
+            sRow++;
+            eRow--;
+            sCol++;
+            eCol--;
         }
+
         return list;
     }
 }
