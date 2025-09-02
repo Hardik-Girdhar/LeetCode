c25fa6ad037f1c83f37ac5deb3003bed36b608be
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-09-02 22:38:42 +0530

    Time: 0 ms (100%), Space: 42.7 MB (10.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,33 +1,62 @@
-// row wise binary search check target between 1st element of row and last
+// Flattering 2d into 1d then binary search by getting formula for i,j from mid = O(logmn)
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-        for(int i=0; i<m; i++){
-            if(matrix[i][0] <= target && matrix[i][n-1] >= target){
-                return binarySearch(matrix[i], target);
-            }
-        }
-        return false;
-    }
-    public static boolean binarySearch(int[] matrix, int target){
+
         int s = 0;
-        int e = matrix.length-1;
+        int e = m*n-1;
+
         while(s<=e){
-            int mid = s+(e-s)/2;
-            if(matrix[mid] == target){
+            int mid = s + (e-s)/2;
+            int row = mid/n;
+            int col = mid%n;
+
+            if(matrix[row][col] == target){
                 return true;
             }
-            else if(matrix[mid] < target){
+            else if(matrix[row][col] < target){
                 s = mid+1;
             }
             else{
-                e=mid-1;
+                e = mid-1;
             }
         }
         return false;
     }
 }
+
+
+// //row wise binary search check target between 1st element of row and last -- O(n + logn) = n
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+//         for(int i=0; i<m; i++){
+//             if(matrix[i][0] <= target && matrix[i][n-1] >= target){
+//                 return binarySearch(matrix[i], target);
+//             }
+//         }
+//         return false;
+//     }
+//     public static boolean binarySearch(int[] matrix, int target){
+//         int s = 0;
+//         int e = matrix.length-1;
+//         while(s<=e){
+//             int mid = s+(e-s)/2;
+//             if(matrix[mid] == target){
+//                 return true;
+//             }
+//             else if(matrix[mid] < target){
+//                 s = mid+1;
+//             }
+//             else{
+//                 e=mid-1;
+//             }
+//         }
+//         return false;
+//     }
+// }
 
 
 // // brute force --> O(n2)
