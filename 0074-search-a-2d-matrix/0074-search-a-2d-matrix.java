11b27629b920e1b10d38e9ee0ccec83494fdb69f
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-09-02 22:34:29 +0530

    Time: 0 ms (100%), Space: 42.2 MB (66.05%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,12 +1,45 @@
+// row wise binary search check target between 1st element of row and last
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == target) return true;
+            if(matrix[i][0] <= target && matrix[i][n-1] >= target){
+                return binarySearch(matrix[i], target);
+            }
+        }
+        return false;
+    }
+    public static boolean binarySearch(int[] matrix, int target){
+        int s = 0;
+        int e = matrix.length-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(matrix[mid] == target){
+                return true;
+            }
+            else if(matrix[mid] < target){
+                s = mid+1;
+            }
+            else{
+                e=mid-1;
             }
         }
         return false;
     }
 }
+
+
+// // brute force --> O(n2)
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+//         for(int i=0; i<m; i++){
+//             for(int j=0; j<n; j++){
+//                 if(matrix[i][j] == target) return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
