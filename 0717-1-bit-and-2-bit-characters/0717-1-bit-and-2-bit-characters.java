ebacaa4bca3f5b6628c631ed4b74e0921d900b5b
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-11-18 15:43:48 +0530

    Time: 0 ms (100%), Space: 44.6 MB (5.33%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
@@ -1,19 +1,45 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
-        int i=0;
-        int n = bits.length;
-        if(n == 1)return true;
-        while(i < n){
-            if(bits[i] == 1){
-                i+=2;
-            }else{
-                i++;
-            }
+        int continous1 = 0;   // if 1s are even -> true, odd -> false,  if any 0 on left of n-1 = true
 
-            if(i == n-1){
-                return true;
+        int n = bits.length;
+
+        if(n==1) return true;
+
+        if(bits[n-2] == 0) return true;
+        
+        for(int i=n-2; i>=0; i--){
+            if(bits[i] == 1){
+                continous1++;
+            }else{
+                break;
             }
         }
-        return false;
+        if(continous1 % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
+
+
+// class Solution {
+//     public boolean isOneBitCharacter(int[] bits) {
+//         int i=0;
+//         int n = bits.length;
+//         if(n == 1)return true;
+//         while(i < n){
+//             if(bits[i] == 1){
+//                 i+=2;
+//             }else{
+//                 i++;
+//             }
+
+//             if(i == n-1){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
