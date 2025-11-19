f58445f9c7c4fa2b720a18d10837416e2751a30a
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-11-19 15:21:01 +0530

    Time: 3 ms (69.35%), Space: 46.5 MB (7.36%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -1,31 +1,46 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
-        Arrays.sort(nums);
-        
-        while(true){
-            boolean found = false;
-            int s=0;
-            int e=nums.length-1;
-            while(s<=e){
-                int mid = s + (e-s)/2;
-                if(nums[mid] == original){
-                    found = true;
-                    break;
-                }
-                else if(nums[mid] < original){
-                    s = mid + 1;
-                }else{
-                    e = mid-1;
-                }
-            }
-
-            if(found){
-                original *= 2;
-            }else{
-                break;
-            }
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
         }
 
+        while(set.contains(original)){
+            original *= 2;
+        }
         return original;
     }
 }
+
+
+// class Solution {
+//     public int findFinalValue(int[] nums, int original) {
+//         Arrays.sort(nums);
+        
+//         while(true){
+//             boolean found = false;
+//             int s=0;
+//             int e=nums.length-1;
+//             while(s<=e){
+//                 int mid = s + (e-s)/2;
+//                 if(nums[mid] == original){
+//                     found = true;
+//                     break;
+//                 }
+//                 else if(nums[mid] < original){
+//                     s = mid + 1;
+//                 }else{
+//                     e = mid-1;
+//                 }
+//             }
+
+//             if(found){
+//                 original *= 2;
+//             }else{
+//                 break;
+//             }
+//         }
+
+//         return original;
+//     }
+// }
