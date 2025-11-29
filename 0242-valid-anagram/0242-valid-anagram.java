39782c464580dae88aba672e7fb83c1ee6c29965
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-11-30 01:47:12 +0530

    Time: 4 ms (78.71%), Space: 46.7 MB (5.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,39 +1,33 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+
+//         if(s.length() != t.length()) return false;
+
+//         int[] arr = new int[26];
+
+//         for(int i=0; i<s.length(); i++){
+//             arr[s.charAt(i) - 'a']++;
+//         }
+//         for(int i=0; i<t.length(); i++){
+//             arr[t.charAt(i) - 'a']--;
+//         }
+
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i] != 0) return false;
+//         }
+
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // HashMap<Character, Integer> map = new HashMap<>();
-        // for(char schar : s){
-        //     map.put(schar , map.getOrDefault(schar, 0) + 1);
-        // }
-        // for(char tchar : t){
-        //     map.put(tchar , map.getOrDefault(tchar, 0) + 1);
-        // }
+        char[] sarr = s.toCharArray();
+        char[] tarr = t.toCharArray();
 
-        // for(int i=0; i<s.length(); i++){
-        //     map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-        //     map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
-        // }
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
 
-        // for(Map.Entry<Character, Integer> entry : map.entrySet()){
-        //     if(entry.getValue() % 2 != 0){
-        //         return false;
-        //     }
-        // }
-        
-        if(s.length() != t.length()) return false;
-
-        int[] arr = new int[26];
-
-        for(int i=0; i<s.length(); i++){
-            arr[s.charAt(i) - 'a']++;
-        }
-        for(int i=0; i<t.length(); i++){
-            arr[t.charAt(i) - 'a']--;
-        }
-
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] != 0) return false;
-        }
-
-        return true;
+        return Arrays.equals(sarr, tarr);
     }
 }
