bd7a8b82085cf57666cdc2596856c44a96bc7a41
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-11-30 01:56:20 +0530

    Time: 18 ms (15.14%), Space: 46.5 MB (14.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -9,11 +9,13 @@ class Solution {
         for(int i=0; i<t.length(); i++){
             char ch = t.charAt(i);
             if(map.get(ch) != null){
-                if(map.get(ch) == 1){
-                    map.remove(ch);
-                }else{
-                    map.put(ch, map.get(ch)-1);
-                }
+                map.put(ch, map.get(ch)-1);
+                if(map.get(ch) == 0) map.remove(ch);
+                // if(map.get(ch) == 1){
+                //     map.remove(ch);
+                // }else{
+                //     map.put(ch, map.get(ch)-1);
+                // }
             }else{
                 return false;
             }
