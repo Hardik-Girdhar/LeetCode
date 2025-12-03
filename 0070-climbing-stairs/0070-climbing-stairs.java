2c1d44f2e63fdb9f851a96edc2da42e545979ffd
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-12-03 17:05:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (23.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        return helper(n, memo);
+        int memo[] = new int[n+1];
+        return solve(n, memo);
     }
 
-    private int helper(int n, int[] memo) {
-        if (n <= 1) return 1;
+    private int solve(int n, int[] memo){
+        if(n<=1) return 1;
 
-        if (memo[n] != 0) return memo[n];
+        if(memo[n] != 0) return memo[n];
 
-        memo[n] = helper(n - 1, memo) + helper(n - 2, memo);
+        memo[n] = solve(n-1, memo) + solve(n-2, memo);
         return memo[n];
     }
 }
-
 
 
 
