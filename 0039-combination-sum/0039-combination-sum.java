8bd138973bb0cfc796e8097cce8b907a9d2964b0
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2026-02-01 18:52:38 +0530

    Time: 2 ms (95.16%), Space: 45.2 MB (99.31%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,13 +1,54 @@
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>> ans= new ArrayList<>();
+//         List<Integer> list = new ArrayList<>();
+
+//         backTrack(0, candidates, target, list, ans);
+//         return ans;
+//     }
+
+//     private void backTrack(int idx, int[] nums, int target, List<Integer> list, List<List<Integer>> ans){
+//         // if(idx == nums.length){
+//         //     if(target == 0){
+//         //         ans.add(new ArrayList<>(list));
+//         //     }
+//         //     return;
+//         // }
+
+//         // if(nums[idx] <= target){
+//         //     list.add(nums[idx]);
+//         //     backTrack(idx, nums, target-nums[idx], list, ans);
+//         //     list.remove(list.size() -1);
+//         // }
+//         // backTrack(idx+1, nums, target, list, ans);
+
+//         if(target == 0){
+//             ans.add(new ArrayList<>(list));
+//             return;
+//         }
+
+//         for(int i=idx; i<nums.length; i++){
+//             if(target < nums[i]) continue;
+
+//             list.add(nums[i]);
+//             backTrack(i, nums, target-nums[i], list, ans);
+//             list.remove(list.size()-1);
+//         }
+//     }
+// }
+
+
+
+
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans= new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-
         backTrack(0, candidates, target, list, ans);
         return ans;
     }
 
-    private void backTrack(int idx, int[] nums, int target, List<Integer> list, List<List<Integer>> ans){
+    public void backTrack(int idx, int nums[], int target, List<Integer> list, List<List<Integer>> ans){
         if(idx == nums.length){
             if(target == 0){
                 ans.add(new ArrayList<>(list));
@@ -16,10 +57,9 @@ class Solution {
         }
 
         if(nums[idx] <= target){
-            list.add(nums[idx]);
-            backTrack(idx, nums, target-nums[idx], list, ans);
-            list.remove(list.size() -1);
-        }
+        list.add(nums[idx]);
+        backTrack(idx, nums, target-nums[idx], list, ans);
+        list.remove(list.size()-1); }
         backTrack(idx+1, nums, target, list, ans);
     }
 }
