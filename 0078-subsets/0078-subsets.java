5f0e225d9e14f68166372a0ef13575fcf5f9c07f
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2026-02-01 19:32:02 +0530

    Time: 0 ms (100%), Space: 44.3 MB (32.3%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -8,15 +8,24 @@ class Solution {
     }
 
     private void backTrack(int idx, int[] nums, List<Integer> list, List<List<Integer>> ans){
-        if(idx >= nums.length){
-            ans.add(new ArrayList<>(list));
-            return;
-        }
+        // if(idx >= nums.length){
+        //     ans.add(new ArrayList<>(list));
+        //     return;
+        // }
 
-        list.add(nums[idx]);
-        backTrack(idx+1, nums, list, ans);
-        list.remove(list.size()-1);
-        backTrack(idx+1, nums, list, ans);
+        // list.add(nums[idx]);
+        // backTrack(idx+1, nums, list, ans);
+        // list.remove(list.size()-1);
+        // backTrack(idx+1, nums, list, ans);
+
+        ans.add(new ArrayList<>(list));
+        // return;
+
+        for(int i=idx; i<nums.length; i++){
+            list.add(nums[i]);
+            backTrack(i+1, nums, list, ans);
+            list.remove(list.size()-1);
+        }
     }
 }
 
